3f9b8f6327a513bfebaf5ea35c34d783fee1b693
taishin <ok220259@oks.iput.ac.jp>
2025-04-10 23:42:07 +0900

    db

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 # Create your models here.
@@ -8,3 +9,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Schedule(models.Model):
+    '''スケジュール'''
+    summary = models.CharField('概要',max_length=50)
+    description = models.TextField('詳細な説明', blank=True)
+    start_time = models.TimeField('開始時刻', default=datetime.time(7,0,0,))
+    end_time = models.TimeField('終了時刻', default=datetime.time(7,0,0))
+    date = models.DateField('日付') 
+    create_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+        return self.summary
