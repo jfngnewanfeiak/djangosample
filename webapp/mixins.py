6a6cee5cec8c235043817223b5f9db82f611ccfb
taishin <ok220259@oks.iput.ac.jp>
2025-04-11 12:26:54 +0900

    カレンダー機能の追加

--- webapp/mixins.py
+++ webapp/mixins.py
@@ -1,5 +1,7 @@
 import calendar
 from collections import deque
+import datetime
+import itertools
 
 class BaseCalendarMixin:
     '''カレンダー関連Mixinの基底クラス'''
@@ -19,3 +21,107 @@ class BaseCalendarMixin:
         week_names = deque(self.week_names)
         week_names.rotate(-self.first_weekday)
         return week_names
+
+class MonthCalendarMixin(BaseCalendarMixin):
+    '''月間カレンダ'''
+    def get_previous_month(self, date):
+        if date.month == 1:
+            return date.replace(year=date.year-1, month=12, day=1)
+        else:
+            return date.replace(month=date.month-1, day=1)
+    
+    def get_next_month(self, date):
+        if date.month == 12:
+            return date.replace(year=date.year+1, month=1, day=1)
+        else:
+            return date.replace(month=date.month+1, day=1)
+    
+    def get_month_days(self, date):
+        '''その月のすべての日を返す'''
+        return self._calendar.monthdatescalendar(date.year, date.month)
+    
+    def get_current_month(self):
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        if month and year:
+            month = datetime.date(year=int(year), month=int(month), day=1)
+        else:
+            month = datetime.date.today().replace(day=1)
+        return month
+    
+    def get_month_calendar(self):
+        '''月間カレンダーの辞書型を返す'''
+        self.setup_calendar()
+        current_month = self.get_current_month()
+        calendar_data = {
+            'now': datetime.date.today(),
+            'month_days': self.get_month_days(current_month),
+            'month_current': current_month,
+            'month_previous': self.get_previous_month(current_month),
+            'month_next': self.get_next_month(current_month),
+            'week_names': self.get_week_names(),
+        }
+        return calendar_data
+    
+class WeekCalendarMixin(BaseCalendarMixin):
+    '''週間カレンダーの機能を提供する'''
+
+    def get_week_days(self):
+        '''その日のすべてを返す'''
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        day = self.kwargs.get('day')
+        if month and year and day:
+            date = datetime.date(year=int(year), month=int(month), day=int(day))
+        else:
+            date = datetime.date.today()
+
+        for week in self._calendar.monthdatescalendar(date.year, date.month):
+            if date in week:
+                return week
+    
+    def get_week_calendar(self):
+        self.setup_calendar()
+        days = self.get_week_days()
+        first = days[0]
+        last = days[-1]
+        calendar_data = {
+            'now': datetime.date.today(),
+            'week_days': days,
+            'week_previous': first - datetime.timedelta(days=7),
+            'week_next': first + datetime.timedelta(days=7),
+            'week_names': self.get_week_names(),
+            'week_first': first,
+            'week_last' : last,
+        }
+        return calendar_data
+
+class WeekWithScheduleMixin(WeekCalendarMixin):
+    '''スケジュール機能つきカレンダー'''
+
+    def get_week_schedules(self, start, end, days):
+        '''それぞれのひ、スケジュールを返す'''
+        lookup = {
+            '{}__range'.format(self.date_field): (start, end)
+        }
+
+        queryset = self.model.objects.filter(**lookup)
+
+        day_schedules = {day: [] for day in days}
+
+        for schedule in queryset:
+            schedule_date = getattr(schedule, self.date_field)
+            day_schedules[schedule_date].append(schedule)
+        
+        return day_schedules
+        
+    def get_week_calendar(self):
+        calendar_context = super().get_week_calendar()
+        calendar_context['week_day_schedules'] = self.get_week_schedules(
+            calendar_context['week_day_schedules'],
+            calendar_context['week_first'],
+            calendar_context['week_last'],
+            calendar_context['week_days']
+        )
+        
+        return calendar_context
